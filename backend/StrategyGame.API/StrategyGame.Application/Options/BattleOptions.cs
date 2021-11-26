fc68ffd784f270c4dd020c3869f7c101c781e88d
Laki Balázs <twarex01@gmail.com>
2021-11-26 12:57:56 +0100

    TODOs

---
+++ backend/StrategyGame.API/StrategyGame.Application/Options/BattleOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyGame.Application.Options
+{
+    public class BattleOptions
+    {
+        public int AttackTime { get; set; }
+    }
+}
